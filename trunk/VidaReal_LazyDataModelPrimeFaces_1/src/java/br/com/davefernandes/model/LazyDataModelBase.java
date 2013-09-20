4757695646c7172bc033fd1d62a408216f53d283
dave.analista@gmail.com <dave.analista@gmail.com@1c3ae632-7b17-9d58-5264-910e3f76c87a>
2013-09-20 22:43:46 +0000

    -melhorei o bixo -corrigi um bug de contagem errada -rodando e sem bugs aparentes

--- trunk/VidaReal_LazyDataModelPrimeFaces_1/src/java/br/com/davefernandes/model/LazyDataModelBase.java
+++ trunk/VidaReal_LazyDataModelPrimeFaces_1/src/java/br/com/davefernandes/model/LazyDataModelBase.java
@@ -7,11 +7,15 @@ package br.com.davefernandes.model;
 import br.com.davefernandes.ejb.AbstractFacade;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.primefaces.model.LazyDataModel;
 import org.primefaces.model.SortOrder;
 
 public class LazyDataModelBase<T> extends LazyDataModel<T> {
 
+    private final static Logger log = Logger.getLogger(LazyDataModelBase.class.getName());
+    // Always use the classname, this way you can refactor
     private volatile AbstractFacade<T> facade;
 
     public LazyDataModelBase(AbstractFacade<T> facade) {
@@ -31,16 +35,16 @@ public class LazyDataModelBase<T> extends LazyDataModel<T> {
     public List<T> load(int first, int pageSize, String sortField, SortOrder sortOrder, Map<String, String> filters) {
         List<T> list = facade.load(first, pageSize, sortField, sortOrder, filters);
 
+        log.log(Level.INFO, "list.size():{0}", String.valueOf(list.size()));
+
         if (list == null) {
             setRowCount(0);
             //throw new RuntimeException("Problem.");
         } else {
-            setRowCount(list.size());
+            setRowCount(facade.count());
         }
         // I am using the following line for debugging:
         // throw new RuntimeException(list.toString());
         return list;
     }
-    
-    
 }
